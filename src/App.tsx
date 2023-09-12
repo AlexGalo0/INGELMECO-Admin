9777972dd884d9670c927cbc63ecf5db85079b4d
Alex Galo <alexgalo246@gmail.com>
2023-09-11 22:31:22 -0600

    iniciando stilamiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AdminForm } from "./Components/AdminForm";
 import { ProductForm } from "./Components/ProductForm";
+import { LoginAdmin } from "./pages/LoginAdmin";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AdminForm />} />
+          <Route path="/" element={<LoginAdmin />} />
           <Route path="/admin/add-product" element={<ProductForm />} />
         </Routes>
       </BrowserRouter>
