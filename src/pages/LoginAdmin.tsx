ff55f3e353c267c7d2febb95171b8a44541cf820
Alex Galo <alexgalo246@gmail.com>
2023-10-12 17:33:12 -0600

    quitando stilos

--- src/pages/LoginAdmin.tsx
+++ src/pages/LoginAdmin.tsx
@@ -3,11 +3,9 @@ import { AdminForm } from "../Components/AdminForm";
 export const LoginAdmin = () => {
   return (
     <>
-      <div className="bar"></div>
-      <div className="container d-flex flex-column  ">
+      
         <AdminForm />
-      </div>
-      <div className="bar"></div>
+     
     </>
   );
 };
