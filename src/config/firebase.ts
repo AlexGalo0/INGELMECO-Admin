eff22fb19ee8d4573a007332b96f8e43ba1c917e
Alex Galo <alexgalo246@gmail.com>
2023-11-13 20:49:36 -0600

    cambios de nuevo

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,11 +5,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 
